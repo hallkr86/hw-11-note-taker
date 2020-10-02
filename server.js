e24ed0a257d9ded3b732f1b999124f9e99084e42
Kesha Hall <k_renee10@yahoo.com>
2020-10-02 18:31:00 -0400

    tried to connect my index and server.js

--- server.js
+++ server.js
@@ -3,30 +3,29 @@ const app = express();
 const fs = require("fs");
 const port = process.env.PORT || 8080;
 const path = require('path');
-
-
+const db = require("./db/db.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-
-require("./public/assets/js/index");
+//require("./public/assets/js/index.js")(app);
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/public/index.html'));
+// });
 
 app.get('/notes', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/notes.html'));
 })
 
 app.get('/api/notes', function(req, res) {
-    fs.readFile('./db.json', 'utf-8', function(err, data) {
+    fs.readFile('./db/db.json', 'utf-8', function(err, data) {
         if (err){
             console.log(err);
             return res.status(500).json({
@@ -42,16 +41,17 @@ app.get('/api/notes', function(req, res) {
         });
     })
 })
-  
+
 app.post('/api/notes', function(req, res) {
     console.log(req.body);
-    fs.readFile('./db.json', 'utf-8', function(err, data) {
+    fs.readFile('./db/db.json', 'utf-8', function(err, data) {
         if (err) throw err;
         console.log(data);
         const updatedData = JSON.parse(data);
-        updatedData.push(req.body);
-        console.log(updatedData);
-        fs.writeFile('./db.json', JSON.stringify(updatedData), function(err) {
+        db.push(req.body);
+        //updatedData.push(req.body);
+        //console.log(updatedData);
+        fs.writeFile('./db/db.json', JSON.stringify(db), function(err) {
         if (err) throw err;
         res.json({
             error: false,
@@ -61,6 +61,10 @@ app.post('/api/notes', function(req, res) {
         });
     });
 });
+
+app.delete('/api/notes', function (req, res) {
+    res.send('Your note has been deleted')
+  });
 
 
 app.listen(port, () => {
